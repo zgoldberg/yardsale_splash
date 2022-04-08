184c8e9b8da4d97d531662fa25162ae1d20c287c
Zach Goldberg <zachgold@seas.upenn.edu>
2022-04-08 15:56:40 -0400

    added regex email validation

--- src/EmailForm.js
+++ src/EmailForm.js
@@ -7,8 +7,16 @@ function EmailForm(props) {
   const [emailData, setEmailData] = useState("");
   const [messageCode, setMessageCode] = useState(0);
 
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
   const handleRequestInvite = () => {
-    if (emailData) {
+    if (emailData && validateEmail(emailData)) {
       $.ajax({
         // url: `http://localhost:5000/email?email=${emailData}`,
         url: `https://zgtodaysyardsale.pythonanywhere.com/email?email=${emailData}`,
@@ -23,14 +31,20 @@ function EmailForm(props) {
           setEmailData("");
         }
       });
+    } else if (emailData) {
+      setMessageCode(2);
     }
   }
 
   const class_prefix = props.mobile ? "mobile-" : "";
   const email_message_text = "Success :)"
+  const invalid_email_message_text = "Invalid email :("
   const email_message = (messageCode == 1) ? (
     <p className={class_prefix + "email-message"}>{email_message_text}</p>
   ) : (
+    (messageCode == 2) ? (
+      <p className={class_prefix + "email-message"} style={{color: "red"}}>{invalid_email_message_text}</p>
+    ) :
     <></>
   );
 
