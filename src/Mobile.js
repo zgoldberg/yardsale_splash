4ba4e7935041a47e25e9c54242073c263525597b
Zach Goldberg <zachgold@seas.upenn.edu>
2022-04-28 17:26:24 -0400

    new brand images

--- src/Mobile.js
+++ src/Mobile.js
@@ -5,14 +5,18 @@ import yardsale_logo from './images/yardsale_logo.png'
 import top_image from './images/top_image.png'
 import bottom_image from './images/bottom_image.png'
 
-import girlfriend_logo from './images/brands/girlfriend-collective.png'
-import johnston_logo from './images/brands/johnston-of-elgin.png'
-import organic_logo from './images/brands/organic-basics.png'
-import pangia_logo from './images/brands/pangia.png'
+import girlfriend_logo from './images/brands/girlfriend_collective.png'
+import johnston_logo from './images/brands/johnstons_of_elgin.png'
+import nudie_logo from './images/brands/nudie_jeans.png'
+import organic_logo from './images/brands/organic_basics.png'
+import older_logo from './images/brands/older_brother.png'
+import pangia_logo from './images/brands/pangaia.png'
+import richer_poorer_logo from './images/brands/richer_poorer.png'
 import satta_logo from './images/brands/satta.png'
-import stellamccartney_logo from './images/brands/stellamccartney.svg'
-import story_logo from './images/brands/story.png'
+import stellamccartney_logo from './images/brands/stella_mccartney.png'
+import story_logo from './images/brands/story_mfg.png'
 import veja_logo from './images/brands/veja.png'
+import wawwa_logo from './images/brands/wawwa.png'
 
 import instagram_logo from './images/instagram_logo.svg'
 
@@ -29,12 +33,16 @@ function Mobile() {
   const brands = [
     [girlfriend_logo, "https://girlfriend.com/"],
     [johnston_logo, "https://www.johnstonsofelgin.com/"],
+    [nudie_logo, "https://www.nudiejeans.com/"],
     [organic_logo, "https://organicbasics.com/"],
+    [older_logo, "https://olderbrother.us/"],
     [pangia_logo, "https://thepangaia.com/"],
+    [richer_poorer_logo, "https://richer-poorer.com/"],
     [satta_logo, "https://www.sattalivity.com/"],
     [stellamccartney_logo, "https://www.stellamccartney.com/"],
     [story_logo, "https://www.storymfg.com/"],
     [veja_logo, "https://www.veja-store.com/en_us/"],
+    [wawwa_logo, "https://wawwaclothing.com/"],
   ];
 
   return (
